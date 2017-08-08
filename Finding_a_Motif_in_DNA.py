55bc688ee1d66aa747832fb0884dcf558fbb7427
sidgampa1 <siddharth_gampa@yahoo.com>
2017-08-07 19:14:04 -0700

    finished finding a motif in dna

--- Finding_a_Motif_in_DNA.py
+++ Finding_a_Motif_in_DNA.py
@@ -6,17 +6,9 @@ motif = sys.argv[2]
 motif_indices = []
 
 counter = 0
-motif_counter = 0
 while (counter < len(DNA)):
-    base = DNA[counter]
-    if (base == motif[motif_counter]):
-        if (motif_counter == len(motif) - 1):
-            motif_indices.append(counter - len(motif) + 2)
-            motif_counter = 0
-        else:
-            motif_counter += 1
-    else:
-        motif_counter = 0
+    if (DNA[counter:(counter + len(motif))] == motif):
+        motif_indices.append(counter + 1) 
     counter += 1
 
 print(str(motif_indices))
